f7c6208635f20fdbe385bd52d300c0cb91aaaafe
samuelyao314 <732834509@qq.com>
2019-06-27 00:28:00 +0800

    3rd, add cjson, cmsgpack

--- examples/service/test/init.lua
+++ examples/service/test/init.lua
@@ -25,8 +25,31 @@ local function test_zset()
         zs:add(score, name)
     end
 
+    skynet.error("=============== zset ================")
     skynet.error("rank 28:", zs:rank("a28"))
     skynet.error("rev rank 28:", zs:rev_rank("a28"))
+    skynet.error("")
+end
+
+
+local function test_cjson()
+    local cjson = require "cjson"
+    local sampleJson = [[{"age":"23","testArray":{"array":[8,9,11,14,25]},"Himi":"himigame.com"}]];
+    local data = cjson.decode(sampleJson)
+     
+    skynet.error("=============== cjson ================")
+    skynet.error("age:", data["age"])
+    skynet.error("array[1]:", data.testArray.array[1])
+end
+
+
+local function test_msgpack()
+    local cmsgpack = require "cmsgpack"
+    local a = {a1 = 1, a2 = 1, a3 = 1, a4 = 1, a5 = 1, a6 = 1, a7 = 1, a8 = 1, a9 = 1}
+    local encode = cmsgpack.pack(a)
+
+    skynet.error("=============== cmsgpack================")
+    skynet.error("a: ", encode)
 end
 
 skynet.start(function()
@@ -34,5 +57,6 @@ skynet.start(function()
     skynet.error("Be water my friend.")
 
     test_zset()
-
+    test_cjson()
+    test_msgpack()
 end)
